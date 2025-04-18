f14ccc945694c6ab79aae7e5930db36c0b1260bc
tajwarsultan <tajwar.sultan00@gmail.com>
2025-04-18 22:03:11 +0500

    ... eslint warning

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,12 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    rules: {
+      "@typescript-eslint/no-explicit-any": ["warn", { ignoreRestArgs: true }],
+    },
+  },
 ];
 
 export default eslintConfig;
